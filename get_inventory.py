0c22f8e01136c8b2ce2d73582d8ab2c1778bcaad
cgayraud <141192P@ssw0rd>
2020-06-24 16:26:25 +0200

    ok

--- get_inventory.py
+++ get_inventory.py
@@ -37,7 +37,7 @@ class ExampleInventory(object):
     def example_inventory(self):
         return {
             'group': {
-                'hosts': ['51.68.28.179' ],
+                'hosts': ['164.132.182.45' ],
                 'vars': {
                     'ansible_ssh_user': 'centos',
                     'ansible_ssh_private_key_file':
